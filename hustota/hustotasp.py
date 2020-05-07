66385297b4f11e838b77179a6df622b9c95c1488
šimon binko <binkosimon@gmail.com>
2020-05-07 10:11:50 +0200

    1.1

--- hustota/hustotasp.py
+++ hustota/hustotasp.py
@@ -1,3 +1,10 @@
-def sp(V, m):
-    h = V / m
-    print(h)
+def sp(V, m, h):
+    if h == "":
+        h = V / m
+        print(h)
+    elif m == "":
+        m = h * V
+        print(m)
+    elif V == "":
+        V = m / h
+        print(V)
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import hustota.hustotamain
 import hustota.hustotasp
 
 hustota.hustotamain.getHustota("Nikl")
-hustota.hustotasp.sp(20, 5)
+hustota.hustotasp.sp("", 5, 10)
